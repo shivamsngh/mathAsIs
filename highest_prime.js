2e1205841d6b844c7d4dad91786a68268981e57e
Shivam Singh <singh.shivam@tftus.com>
2018-09-21 17:38:13 +0530

    Added new algos

--- highest_prime.js
+++ highest_prime.js
@@ -1,17 +1,30 @@
 const largestPrime = (num) => {
-    // divide the number by half
-    let checkmax = parseInt(num / 2);
-    // start from last prime num
-    for (let i = checkmax; i > 1; i--) {
-        if (isPrime(i)) {
-            if (num % i === 0)
-                return i;
+    if (isPrime(num)) {
+        return num;
+    }
+    else {
+        // divide the num in half and check
+        let checkmax = parseInt(num / 2);
+        for (let i = checkmax; i > 1; i--) {
+            if (isPrime(i)) {
+                if (num % i === 0)
+                    return i;
+            }
         }
+
     }
 }
 
+// Quadratic Sieve
+
+
+// pollard's rho
+
+
+
 const isPrime = (num) => {
-    for (let i = 2; i <= parseInt(num / 2); ++i) {
+    let checkMax = parseInt(Math.sqrt(num));
+    for (let i = 2; i <= checkMax; ++i) {
         if (num % i === 0) {
             return false;
         }
@@ -19,4 +32,4 @@ const isPrime = (num) => {
     return true;
 }
 
-console.log("Highest prime is:", largestPrime(600851475143));
+console.log("Highest prime is:", largestPrime(13195));
